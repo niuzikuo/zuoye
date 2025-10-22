6b48f16dfc124bb663f99ba0814ea2060eca4788
niuzikuo <1429756913@qq.com>
2025-10-22 14:48:33 +0800

    Add files via upload

--- FileName.c
+++ FileName.c
@@ -1,12 +1,19 @@
 #include<stdio.h>
-int main() {
-	int a;
-	double b, c, d;
-	b = 1.0;
-	d = 0.0;
-	for (a = 2; a <= 4; a++) {
-		c = b / a;
-		d = c + d;
-	}printf("和为%f", d);
+	int max(int a, int b, int c) {
+		int d;
+		if (a >= b && a >= c)
+			d = a;
+		else if (b >= a && b >= c)
+			d = b;
+		else
+			d = c;
+		return d;
+	}
+	int main()
+	{
+		int a, b, c, d;
+	scanf_s("%d%d%d", &a, &b, &c);
+	d = max(a, b, c);
+	printf("a,b,c最大的数是%d", d);
 	return 0;
 }
